85c9f761d25e035de1655ecdf3dbd9a9a9d0ad32
jodiecal-hub <jodie.callaghan@mbhci.nhs.uk>
2026-06-18 11:23:54 +0000

    end of morning course

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,30 @@
 /*
-Example lesson stub
+My First Sequel Lesson
+Name: Jodie Callaghan
+Date: 18-06-2026
 */
 
+--Looking at surgical activity--
+
 SELECT
-	*
+	ps.PatientId
+	
+	,ps.Hospital
+	
+	,ps.Ward
+	
+	,DATEADD(WEEK, -2, ps.AdmittedDate) AS ReminderDate
+	
+	,ps.AdmittedDate
+	
+	,ps.DischargeDate
+	
+	,ps.Tariff
 FROM
-	PatientStay ps ;
+	PatientStay ps
+WHERE ps.Hospital IN ('kingston', 'pruh')
+	AND ps.Ward LIKE '%surgery'
+	AND ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-02-29'
+
+SELECT
+	DATEADD(WEEK, -2, '2026-06-18')
